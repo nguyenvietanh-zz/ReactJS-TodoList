852a61cafbd47171b93af71d278e800ad84029ef
nguyenvietanh <nguyenvietanh168@gmail.com>
2021-03-09 10:53:34 +0700

    Bước 1

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskName: "", tasks: [] };
+  }
+
+  myTaskChangeHandler = (event) => {
+    this.setState({ taskName: event.target.value });
+  };
+
+  addTask = () => {
+    if(this.state.taskName === ''){
+      return
+    }
+    this.state.tasks.push( this.state.taskName);
+    
+    this.setState({ taskName: "" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+      
+          <br />
+          Todo List
+          <br />
+          <div className="aligned">
+            <img
+              src="./assets/iconfinder_plus-add-new-create-attach-maximize_2931155.png"
+              alt="Add Task"
+              width="25"
+              style={{ cursor: "pointer" }}
+              title="Bấm để thêm task"
+              onClick={() => this.addTask()}
+            />
+            <input
+              type="text"
+              value={this.state.taskName}
+              onChange={this.myTaskChangeHandler}
+            />
+          </div>
+  
+
+            <ul>
+              {this.state.tasks.map((value, index) => {
+                return <li key={index} >{value}</li>;
+              })}
+            </ul>
+      
+      </div>
+    );
+  }
 }
-
 export default App;
